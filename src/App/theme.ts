0b33019879e47725d4fc8d05e515b0acccaa3fbb
Parth V <26404957+Parthvsquare@users.noreply.github.com>
2024-05-09 04:22:12 +0530

    refactor: updated theme primary

--- src/App/theme.ts
+++ src/App/theme.ts
@@ -5,28 +5,23 @@ import {
   getContrastRatio,
 } from "@mui/material/styles";
 
-// Augment the palette to include a violet color
-declare module "@mui/material/styles" {
-  interface Palette {
-    violet: Palette["primary"];
-  }
-
-  interface PaletteOptions {
-    violet?: PaletteOptions["primary"];
-  }
-}
-
 const violetBase = "#7F00FF";
 const violetMain = alpha(violetBase, 0.7);
 
 const customPalette: Record<"palette", PaletteOptions> = {
   palette: {
-    violet: {
+    primary: {
       main: violetMain,
       light: alpha(violetBase, 0.5),
       dark: alpha(violetBase, 0.9),
       contrastText:
         getContrastRatio(violetMain, "#fff") > 4.5 ? "#fff" : "#111",
+    },
+    secondary: {
+      main: "#f44336",
+      light: "#e57373",
+      dark: "#d32f2f",
+      contrastText: "#fff",
     },
   },
 };
