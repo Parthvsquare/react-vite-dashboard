0a88f9285917ad524ff569cbc8807150e04eb6ca
Parth V <26404957+Parthvsquare@users.noreply.github.com>
2024-05-10 05:34:13 +0530

    feat: added env to local storage

--- src/store/jotaiStore.ts
+++ src/store/jotaiStore.ts
@@ -1,8 +1,11 @@
 import { Application, EnvStore } from "@/common/types/custom";
 import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
 const applicationsStore = atom<Application[] | []>([]);
 const selectedApplicationStore = atom<Application | undefined>(undefined);
-const environmentStore = atom<EnvStore[] | []>([]);
+const environmentStore = atomWithStorage<EnvStore[] | []>("myEnv", []);
+
+// const environmentStore = atom<EnvStore[] | []>([]);
 
 export { applicationsStore, selectedApplicationStore, environmentStore };
